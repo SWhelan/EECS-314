184fb357132d2db28c103afdc951c65c427d9cbd
Molly Henderson <meh111@case.edu>
2015-03-23 20:10:01 -0400

    max!

--- main.asm
+++ main.asm
@@ -1,6 +1,6 @@
 main:
 	.data
-question: .ascii "Which function?\nThere are n functions:\n 1 for sum\n 2 for product\n 3 for min\n\ 4 for max\n 5 for average\n -1 to exit program\n\0"
+question: .ascii "Which function?\nThere are n functions. Type:\n\t 1 for sum\n\t 2 for product\n\t 3 for min\n\t 4 for max\n\t 5 for average\n\t 6 for median\n\t 7 for mode\n\t 8 for sort\n\t 9 for matrix multiply\n\t -1 to exit program\n\0"
 numInputs: .ascii "How many parameters?\n\0"
 enterInput: .ascii "Please input your parameters\n\0"
 newLine: .ascii "\n\0"
@@ -10,6 +10,10 @@ productDebug: .ascii "Calculating product.\n\0"
 minDebug: .ascii "Calculating min.\n\0"
 maxDebug: .ascii "Calculating max.\n\0"
 averageDebug: .ascii "Calculating average.\n\0"
+medianDebug: .ascii "Calculating median.\n\0"
+modeDebug: .ascii "Calculating mode.\n\0"
+sortDebug: .ascii "Sorting.\n\0"
+matrixMultDebug: .ascii "Matrix multiplying.\n\0"
 
 	.text
 ask:	li  $v0, 4          # service 4 is print string
@@ -25,8 +29,12 @@ checkFunction:
 	beq $t0, 1, getParamCount
 	beq $t0, 2, getParamCount
 	beq $t0, 3, getParamCount
-	#beq $t0, 4, getParamCount
+	beq $t0, 4, getParamCount
 	beq $t0, 5, getParamCount
+	#beq $t0, 6, getParamCount
+	#beq $t0, 7, getParamCount
+	#beq $t0, 8, getParamCount
+	#beq $t0, 9, matrixMult
 	j end
 
 getParamCount:
@@ -63,8 +71,11 @@ decideFunction:
 	beq $t0, 1, sum
 	beq $t0, 2, product
 	beq $t0, 3, min
-	#beq $t0, 4, max
+	beq $t0, 4, max
 	beq $t0, 5, average
+	#beq $t0, 6, median
+	#beq $t0, 7, mode
+	#beq $t0, 8, sort
 
 ######## Sum
 		
@@ -115,7 +126,7 @@ min:
 	add $t2, $zero, $zero # t2 is loop counter
 	add.d $f8, $f30, $f30 # cleared sum for some reason
 	
-	### Stores Iinfinity in $f8 ###
+	### Stores Infinity in $f8 ###
 	lui $t3, 0x7FF0 
 	ori $t3, 0x0000
 	add $t4, $zero, $zero
@@ -138,6 +149,40 @@ keepMin:
 	sub $t6, $t6, 8 # decrement heap pointer
 	addi $t2, $t2, 1 # increment loop counter
 	j minLoop
+	
+######## Max
+		
+max:	
+	li $v0, 4         # service 4 is print string
+    	la $a0, maxDebug  # load desired value into argument register $a0, using pseudo-op
+	syscall
+	
+	add $t2, $zero, $zero # t2 is loop counter
+	add.d $f8, $f30, $f30 # cleared sum for some reason
+	
+	### Stores -Infinity in $f8 ###
+	lui $t3, 0xFFF0 
+	ori $t3, 0x0000
+	add $t4, $zero, $zero
+	sw $t3, 8($t6)
+	sw $t4, 12($t6)
+	lwc1 $f9, 8($t6)
+	lwc1 $f8, 12($t6)
+	
+maxLoop:
+  	beq $t2, $t1, printAnswer
+	ldc1 $f10, 0($t6) # load the current input into f10
+	
+        c.lt.d $f10, $f8 # set floating point conditional to true if input is less than current max
+        bc1t keepMax # if current min < input don't change the current min
+	
+changeMax:
+	add.d $f8, $f10, $f30 # set the curren max (f8) to the new higher value
+	
+keepMax: 
+	sub $t6, $t6, 8 # decrement heap pointer
+	addi $t2, $t2, 1 # increment loop counter
+	j maxLoop
 	
 ######## Average
 		
